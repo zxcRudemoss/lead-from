abebe359abd7c43b9a74c8edf47bf4464c163dbe
zxcRudemoss <altufievmain@gmail.com>
2024-09-11 14:12:27 +0300

    added curl_setopts CURLOP_POST and CURLOPT_POSTFIELDS and added curl_setopt headers

--- getstatuses.php
+++ getstatuses.php
@@ -1,9 +1,14 @@
 <?php
 $url = 'https://crm.belmar.pro/api/v1/getstatuses';
 $ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);       
+$postData = file_get_contents('php://input');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($ch, CURLOPT_POST, true);            
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postData); 
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'token:ba67df6a-a17c-476f-8e95-bcdb75ed3958'
+    'token:ba67df6a-a17c-476f-8e95-bcdb75ed3958',
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($postData)
 ));
 
 $response = curl_exec($ch);
